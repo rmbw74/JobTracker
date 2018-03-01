b46bf51ee09472579ee7a2c376648077d8719bbf
Ray Medrano <rmbw74@gmail.com>
2018-03-01 11:27:21 -0600

    initial commit

---
+++ JobTracker/JobTracker/Models/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTracker.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Company Name")]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string Street { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string State { get; set; }
+
+        public string Industry { get; set; }
+
+    }
+}
