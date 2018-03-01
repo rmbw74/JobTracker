c6eeca912d6b43ec47040ca10ce60c87a921cb19
Ray Medrano <rmbw74@gmail.com>
2018-03-01 14:22:46 -0600

    Added JobContacts Model

--- JobTracker/JobTracker/Models/JobContacts.cs
+++ JobTracker/JobTracker/Models/JobContacts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace JobTracker.Models
 {
     public class JobContacts
     {
+        [Key]
+        public int Id { get; set; }
+
+        //foreign keys
+        public virtual ICollection<Job> Jobs { get; set; }
+
+        public virtual ICollection<Contact> Contacts { get; set; }
     }
 }
